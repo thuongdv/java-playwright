61218a8dcf17e408a059336669ea6070912c0c13
thuongdangagest <thuong.dang@agest.vn>
2026-04-06 15:55:10 +0700

    Add logger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import java.util.logging.Logger;
+
 /** Hello world! */
 public class App {
+  private static final Logger logger = Logger.getLogger(App.class.getName());
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    String name = "Playwright";
+    logger.info("Hello " + name + "!");
+    logger.info("Hello World!");
   }
 }
